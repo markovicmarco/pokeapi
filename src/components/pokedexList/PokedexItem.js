ceccb129282850065941b4503e97eb1f8348fcb1
andreidem18 <75345854+andreidem18@users.noreply.github.com>
2021-02-26 20:16:12 -0500

    corrección css

--- src/components/pokedexList/PokedexItem.js
+++ src/components/pokedexList/PokedexItem.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import "./pokedexItem.css";
+import "./PokedexItem.css";
 
 const PokedexItem = ({ url }) => {
 
