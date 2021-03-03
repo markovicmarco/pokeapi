8cd3b79720f94e2c7e58635ee7163f239a02f87c
andreidem18 <75345854+andreidem18@users.noreply.github.com>
2021-03-02 23:21:30 -0500

    dark mode

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -1,14 +1,30 @@
-import {isDark, changeDark} from '../../provider/AuthProvider';
+import {useDarkMode} from '../../provider/AuthProvider';
+import "./Settings.css";
 
 const Settings = () => {
 
-  document.body.style = `background: ${isDark? 'black' : 'white'}`;
+  const {isDark, changeDark} = useDarkMode();
+  document.body.style = `background: ${isDark? 'rgb(29, 27, 27)' : 'rgb(253, 253, 253)'}`;
 
   
   return(
-    <div>Settings
-      <input type="checkbox"/>
-    </div>
+    <section 
+    className="container"
+    style={{color: isDark? "white" : "black"}}>
+      <h2 className="title">Settings</h2>
+      <div 
+      className="card-setting"
+      style={{background: isDark ? 'rgb(17, 17, 17)' : '#fff'}}
+      >
+        <h3 className="title">Theme</h3>
+        <div className="check-container center">
+          <span>Light</span>
+            <input type="checkbox" 
+            onChange={() => changeDark()}/>
+          <span>Dark</span>
+        </div>
+      </div>
+    </section>
   )
 }
 
