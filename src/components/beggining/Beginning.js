75c49fce291283a1d214a669b9547ad8e293c4a0
andreidem18 <75345854+andreidem18@users.noreply.github.com>
2021-03-05 23:02:53 -0500

    beginning

---
+++ src/components/beggining/Beginning.js
@@ -0,0 +1,12 @@
+import "./Beginning.css";
+
+const Beggining = () => {
+  return (
+    <div class="beggining">
+      Beggining
+      <img alt="" src="https://static.wikia.nocookie.net/nintendo/images/2/2b/SM_Red.png/revision/latest?cb=20161103045713&path-prefix=en"/>
+    </div>
+  )
+}
+
+export default Beggining;
