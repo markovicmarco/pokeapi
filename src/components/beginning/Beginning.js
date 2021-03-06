9ca0abd06b4aea334cb7286d5af1a98234419dcb
andreidem18 <75345854+andreidem18@users.noreply.github.com>
2021-03-06 10:20:34 -0500

    beginning

--- src/components/beginning/Beginning.js
+++ src/components/beginning/Beginning.js
@@ -2,9 +2,15 @@ import "./Beginning.css";
 
 const Beginning = () => {
   return (
-    <div class="beggining row">
-      <p className="title">Hello <br/> trainer!</p>
-      <img alt="" src="https://static.wikia.nocookie.net/nintendo/images/2/2b/SM_Red.png/revision/latest?cb=20161103045713&path-prefix=en"/>
+    <div class="beggining">
+      <div>
+        <div class="row">
+          <p className="title">Hello trainer!</p>
+          <img alt="" src="https://static.wikia.nocookie.net/nintendo/images/2/2b/SM_Red.png/revision/latest?cb=20161103045713&path-prefix=en"/>
+        </div>
+      </div>
+      <p>Give me your name to start</p>
+      <input type="text"/>
     </div>
   )
 }
