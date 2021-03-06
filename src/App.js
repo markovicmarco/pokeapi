b78184e3d4331d480d92b8bf6283b7bfa18cd699
Andres-R-code <72171067+Andres-R-code@users.noreply.github.com>
2021-03-05 21:53:45 -0500

    footer 2.0

--- src/App.js
+++ src/App.js
@@ -36,18 +36,41 @@ export default function App() {
               </Switch>
               <footer className="footer" >
                 <h2 style={{paddingTop:"30px", textShadow: "2px 2px 2px grey"}}>Create by Developers:</h2>
+                <br />
                 <ul style={{listStyle:"none", textAlign:"center"}}>
-                  <li style={{textShadow: "2px 2px 2px grey"}}>Andres Mendoza  {" "} 
-                    <a href="https://www.linkedin.com/in/andr%C3%A9s-felipe-rojas-valero-44b215176/" target="_black"><i class="fab fa-linkedin zoom" aria-hidden="true"></i></a>
-                    <a href="https://mail.google.com/mail/u/0/?tab=rm&amp;ogbl#inbox" target="_blanck"><i class="fa fa-envelope-o zoom" aria-hidden="true"></i></a>
+                  <li className="contac">Andres Mendoza  {" "} 
+                    <a href="https://www.linkedin.com/in/andrés-david-mendoza-márquez-867a1b175" target="_black">
+                      <i class="fab fa-linkedin zoom" aria-hidden="true"></i>
+                      </a>
+                      <br />
+                       andres.david.mm@hotmail.com
+                    <a href="https://mail.google.com/mail/u/0/#inbox?compose=new" target="_blanck">
+                      <i class="fa fa-envelope-o zoom" aria-hidden="true"></i>
+                      </a>
                   </li>
-                  <li style={{textShadow: "2px 2px 2px grey"}}> Andrés F.   Rojas {" "} 
-                  <a href="https://www.linkedin.com/in/andr%C3%A9s-felipe-rojas-valero-44b215176/" target="_black"><i class="fab fa-linkedin zoom" aria-hidden="true"></i></a>
-                  <a href="https://mail.google.com/mail/u/0/?tab=rm&amp;ogbl#inbox" target="_blanck"><i class="fa fa-envelope-o zoom" aria-hidden="true"></i></a>
+                  <br />
+                  <br />
+                  <li className="contac"> Andrés F.   Rojas {" "} 
+                  <a href="https://www.linkedin.com/in/andr%C3%A9s-felipe-rojas-valero-44b215176/" target="_black">
+                    <i class="fab fa-linkedin zoom" aria-hidden="true"></i>
+                    </a>
+                    <br />
+                    andres.02rojas@gmail.com {" "}
+                  <a href="https://mail.google.com/mail/u/0/#inbox?compose=new" target="_blanck">
+                    <i class="fa fa-envelope-o zoom" aria-hidden="true"></i>
+                    </a>
                   </li>
-                  <li style={{textShadow: "2px 2px 2px rgba(0, 0, 0, 0.404)"}}>Daniel Camacho {" "} 
-                  <a href="https://www.linkedin.com/in/andr%C3%A9s-felipe-rojas-valero-44b215176/" target="_black"><i class="fab fa-linkedin zoom" aria-hidden="true"></i></a>
-                  <a href="https://mail.google.com/mail/u/0/?tab=rm&amp;ogbl#inbox" target="_blanck"><i class="fa fa-envelope-o zoom" aria-hidden="true"></i></a>
+                  <br />
+                  <br />
+                  <li className="contac">Daniel Camacho {" "} 
+                     <a href="https://www.linkedin.com/in/daniel-camacho-82066416b" target="_black">
+                         <i class="fab fa-linkedin zoom" aria-hidden="true"></i>
+                     </a>
+                     <br />
+                     danielk1260@gmail.com {" "}
+                     <a href="https://mail.google.com/mail/u/0/#inbox?compose=new" target="_blanck">
+                        <i class="fa fa-envelope-o zoom" aria-hidden="true"></i>
+                     </a>
                   </li>
                 </ul>
                 <p className="space">
