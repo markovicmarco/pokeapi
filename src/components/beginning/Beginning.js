f726d85cdcaa7ff10886d72e242730d66375e1d0
andreidem18 <75345854+andreidem18@users.noreply.github.com>
2021-03-06 14:54:59 -0500

    beggining design

--- src/components/beginning/Beginning.js
+++ src/components/beginning/Beginning.js
@@ -2,15 +2,18 @@ import "./Beginning.css";
 
 const Beginning = () => {
   return (
-    <div className="beggining">
-      <div>
-        <div className="row">
-          <p className="title">Hello trainer!</p>
+    <div className="beginning container">
+        <div className="banner">
+          <h2 className="title">Hello trainer!</h2>
           <img alt="" src="https://www.seekpng.com/png/full/201-2011786_red-by-xous-54-red-pokemon-trainer-png.png"/>
         </div>
+      <p className="title">Give me your name to start</p>
+      <div>
+        <input type="text"/>
+        <button>
+          <i class="fas fa-paper-plane"></i>
+        </button>
       </div>
-      <p>Give me your name to start</p>
-      <input type="text"/>
     </div>
   )
 }
